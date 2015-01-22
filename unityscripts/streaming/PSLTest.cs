55fbd2c313e46ad18086627df05d81ac10932771
bornold <jonas.bornold@gmail.com>
2015-01-22 12:34:20 +0100

    Added a fake joint localization for testint and a test for showing in unity

---
+++ unityscripts/streaming/PSLTest.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using OTK = OpenTK;
+
+namespace QTM2Unity.Unity
+{
+    public class PSLTest : MonoBehaviour
+    {
+        private List<LabeledMarker> markerData;
+        private RTClient rtClient;
+        private Dictionary<string, Vector3> joints = new Dictionary<string, Vector3>();
+
+        public bool debug = false;
+        public float markerScale = 0.015f;
+        private bool streaming = false;
+
+        // Use this for initialization
+        void Start()
+        {
+            rtClient = RTClient.getInstance();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if ( rtClient == null)
+            {
+                Start();
+                //rtClient = RTClient.getInstance();
+                streaming = false;
+            }
+            if (rtClient.getStreamingStatus() && !streaming)
+            {
+                streaming = true;
+            }
+
+            markerData = rtClient.Markers;
+
+            if (markerData == null && markerData.Count == 0) return;
+
+            joints = new PseudoJointLocalization(markerData).joints.ToDictionary(x => x.Key, x => convertFromSlimDXVector(x.Value));
+            
+        }
+        void OnDrawGizmos()
+        {
+            if (!Application.isPlaying)
+                return;
+            Gizmos.color = Color.cyan;
+
+            // Hip to Left toes
+            if (joints.ContainsKey("Hip") && joints.ContainsKey("LeftHip"))
+                Gizmos.DrawLine(joints["Hip"], joints["LeftHip"]);
+
+            if (joints.ContainsKey("LeftHip") && joints.ContainsKey("LeftKnee"))
+                Gizmos.DrawLine(joints["LeftHip"], joints["LeftKnee"]);
+
+            if (joints.ContainsKey("LeftKnee") && joints.ContainsKey("LeftAnkle"))
+                Gizmos.DrawLine(joints["LeftKnee"], joints["LeftAnkle"]);
+
+            if (joints.ContainsKey("LeftAnkle") && joints.ContainsKey("LeftToes"))
+                Gizmos.DrawLine(joints["LeftAnkle"], joints["LeftToes"]);
+
+            // Hip to Right toes
+            if (joints.ContainsKey("Hip") && joints.ContainsKey("RightHip"))
+                Gizmos.DrawLine(joints["Hip"], joints["RightHip"]);
+
+            if (joints.ContainsKey("RightHip") && joints.ContainsKey("RightKnee"))
+                Gizmos.DrawLine(joints["RightHip"], joints["RightKnee"]);
+
+            if (joints.ContainsKey("RightKnee") && joints.ContainsKey("RightAnkle"))
+                Gizmos.DrawLine(joints["RightKnee"], joints["RightAnkle"]);
+
+            if (joints.ContainsKey("RightAnkle") && joints.ContainsKey("RightToes"))
+                Gizmos.DrawLine(joints["RightAnkle"], joints["RightToes"]);
+
+
+            // Hip to head
+            if (joints.ContainsKey("Hip") ) {
+                if (joints.ContainsKey("Spine")) {
+                    Gizmos.DrawLine(joints["Hip"], joints["Spine"]);
+                }
+                if (joints.ContainsKey("Neck")) {
+                    Gizmos.DrawLine(joints["Hip"], joints["Neck"]);
+                }
+            }
+            
+            if (joints.ContainsKey("Spine") && joints.ContainsKey("Neck")) {
+                    Gizmos.DrawLine(joints["Spine"], joints["Neck"]);
+            }
+
+            if (joints.ContainsKey("Neck") && joints.ContainsKey("Head"))
+                Gizmos.DrawLine(joints["Neck"], joints["Head"]);
+
+
+            // Neck to Right Hand
+            if (joints.ContainsKey("Neck") && joints.ContainsKey("RightShoulder"))
+                Gizmos.DrawLine(joints["Neck"], joints["RightShoulder"]);
+
+            if (joints.ContainsKey("RightShoulder") && joints.ContainsKey("RightElbow"))
+                Gizmos.DrawLine(joints["RightShoulder"], joints["RightElbow"]);
+
+            if (joints.ContainsKey("RightElbow") && joints.ContainsKey("RightWrist"))
+                Gizmos.DrawLine(joints["RightElbow"], joints["RightWrist"]);
+
+            if (joints.ContainsKey("RightWrist") && joints.ContainsKey("RightHand"))
+                Gizmos.DrawLine(joints["RightWrist"], joints["RightHand"]);
+
+
+            // Neck to Left hand
+            if (joints.ContainsKey("Neck") && joints.ContainsKey("LeftShoulder"))
+                Gizmos.DrawLine(joints["Neck"], joints["LeftShoulder"]);
+
+            if (joints.ContainsKey("LeftShoulder") && joints.ContainsKey("LeftElbow"))
+                Gizmos.DrawLine(joints["LeftShoulder"], joints["LeftElbow"]);
+
+            if (joints.ContainsKey("LeftElbow") && joints.ContainsKey("LeftWrist"))
+                Gizmos.DrawLine(joints["LeftElbow"], joints["LeftWrist"]);
+
+            if (joints.ContainsKey("LeftWrist") && joints.ContainsKey("LeftHand"))
+                Gizmos.DrawLine(joints["LeftWrist"], joints["LeftHand"]);
+
+    
+            foreach (KeyValuePair<string,Vector3> kvp in joints)
+            {
+                Gizmos.DrawSphere(kvp.Value, markerScale);   
+            }
+
+        }
+        // TODO write a converter https://msdn.microsoft.com/en-us/library/ayybcxe5.aspx
+        private Vector3 convertFromSlimDXVector(OTK.Vector3 v) {
+            return new Vector3(v.X, v.Y, v.Z);
+        }
+    }
+}
---
+++ unityscripts/streaming/PseudoJointLocalization.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OpenTK;
+
+namespace QTM2Unity.Unity
+{
+    class PseudoJointLocalization
+    {
+        private Dictionary<string, Vector3> markers;
+        public Dictionary<string, Vector3> joints;
+        List<MarkersForJoints> markersForJoints = new List<MarkersForJoints>
+            ()
+                {
+                    new MarkersForJoints("Hip", "L_IAS",  "R_IAS", "SACR"),
+                   // new MarkersForJoints("Spine", "TV12"),
+                    new MarkersForJoints("Neck", "SME", "TV2"),
+                    new MarkersForJoints("Head", "SGL", "L_HEAD", "R_HEAD"),
+
+                    new MarkersForJoints("LeftHip", "L_IAS"),
+                    new MarkersForJoints("LeftKnee", "L_PAS", "L_TTC"),
+                    new MarkersForJoints("LeftAnkle", "L_FAL", "L_FCC"),
+                    new MarkersForJoints("LeftToes", "L_FM2"),
+
+                    new MarkersForJoints("RightHip", "R_IAS"),
+                    new MarkersForJoints("RightKnee", "R_PAS", "R_TTC"),
+                    new MarkersForJoints("RightAnkle", "R_FAL", "R_FCC"),
+                    new MarkersForJoints("RightToes", "R_FM2"),
+
+                    new MarkersForJoints("LeftShoulder", "L_SAE"),
+                    new MarkersForJoints("LeftElbow", "L_UOA", "L_HLE", "L_HME"),
+                    new MarkersForJoints("LeftWrist", "L_RSP", "L_USP"),
+                    new MarkersForJoints("LeftHand", "L_HM2"),
+
+                    new MarkersForJoints("RightShoulder", "R_SAE"),
+                    new MarkersForJoints("RightElbow", "R_UOA", "R_HLE", "R_HME"),
+                    new MarkersForJoints("RightWrist", "R_RSP", "R_USP"),
+                    new MarkersForJoints("RightHand", "R_HM2")
+            };
+
+        public PseudoJointLocalization(List<LabeledMarker> markerData)
+        {
+            markers = markerData.ToDictionary(k => k.label, v => v.position);
+            joints = markersForJoints.ToDictionary(k => k.Name, v => getPoint(v));
+        }
+
+        protected Vector3 getPoint(MarkersForJoints group)
+        {
+            Vector3 value = Vector3.Zero;
+            switch (group.Count)
+            {
+                case 1:
+                    if (markers.ContainsKey(group.Forward))
+                    {
+                        value = markers[group.Forward];
+                    }
+                    break;
+                case 2:
+                    if (markers.ContainsKey(group.Left) ) {
+                        if (markers.ContainsKey(group.Right))
+                        {
+                            value = getMid(
+                            markers[group.Left],
+                            markers[group.Right]);
+                        }
+                        else
+                        {
+                            value = markers[group.Left];
+                        }
+                    }
+                    break;
+                case 3:
+                    if (markers.ContainsKey(group.Forward) &&
+                        markers.ContainsKey(group.Left) &&
+                        markers.ContainsKey(group.Right))
+                    {
+                        value = getMid(
+                                    markers[group.Forward],
+                                    markers[group.Left],
+                                    markers[group.Right]);
+                    }
+                    else if (markers.ContainsKey(group.Left) &&
+                             markers.ContainsKey(group.Right))
+                    {
+                        value = getMid(
+                            markers[group.Left],
+                            markers[group.Right]);
+                    }
+                    break;
+                default: break;
+            }
+            return value;
+        }
+        protected Vector3 getMid(Vector3 leftVect, Vector3 rightVect)
+        {
+            return (leftVect - rightVect) * 0.5f + rightVect;
+        }
+
+        protected Vector3 getMid(Vector3 forwardVect, Vector3 leftVect, Vector3 rightVect) {
+            Vector3 backMid = getMid(leftVect, rightVect);
+            return forwardVect + (backMid - forwardVect) * 2 / 3;
+        }
+
+    }
+
+    public class MarkersForJoints 
+    {
+        private readonly string jointName;
+        private readonly string forwardMarker;
+        private readonly string leftMarker;
+        private readonly string rightMarker;
+        private readonly int numberOfVectors;
+        public MarkersForJoints(string name, string middle) {
+            this.numberOfVectors = 1;
+            forwardMarker = middle;
+            this.jointName = name;
+        }
+        public MarkersForJoints(string name, string left, string right) {
+            this.jointName = name;
+            this.leftMarker = left;
+            this.rightMarker = right;
+            this.numberOfVectors = 2;
+
+        }
+        public MarkersForJoints(string name, string left, string right, string forward) {
+            this.numberOfVectors = 3;
+            this.leftMarker = left;
+            this.rightMarker = right;
+            this.forwardMarker = forward;
+            this.jointName = name;
+
+        }
+        public int Count {
+            get { return numberOfVectors;}
+        }
+        public string Forward {
+            get { return forwardMarker;}
+        }
+        public string Left {
+            get { return leftMarker;}
+        }
+        public string Right {
+            get { return rightMarker;}
+        }
+        public string Name {
+            get { return jointName;}
+        }
+    }
+}
