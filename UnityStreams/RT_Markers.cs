2ab8c9aeff97ff2bc2065013a9be851ccaa7a570
bornold <jonas.bornold@gmail.com>
2015-07-13 13:44:02 +0200

    Somthing crazy

---
+++ UnityStreams/RT_Markers.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+namespace QTM2Unity
+{
+    class RT_Markers : RT
+    {
+        private MarkersPreprocessor mp;
+        protected Dictionary<string, OpenTK.Vector3> markers;
+
+        public override void StartNext()
+        {
+            mp = new MarkersPreprocessor();
+        }
+
+        // Update is called once per frame
+        public override void UpdateNext()
+        {
+            if (mp == null)
+            {
+                UnityEngine.Debug.LogWarning("Reseting");
+                mp = new MarkersPreprocessor();
+                return;
+            }
+            if (!mp.ProcessMarkers(markerData, out markers))
+            {
+                Debug.LogError("markers...");
+                return;
+            }
+        }
+    }
+}
+
