8d6da3ba79ec697cc3c04dac32a18146167bdb3b
Jonas Bornold <jonas.bornold@gmail.com>
2015-08-10 19:12:43 +0200

    Change Rotation Model order

--- Unity Dependent Classes/CharactersRotation.cs
+++ Unity Dependent Classes/CharactersRotation.cs
@@ -11,10 +11,6 @@
     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 #endregion
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using UnityEngine;
 
 namespace QualisysRealTime.Unity.Skeleton
@@ -95,40 +91,6 @@ namespace QualisysRealTime.Unity.Skeleton
     {
         public Model2()
         {
-            root = new Vector3(0f, 0f, 0f);
-            hip = new Vector3(90f, 180f, 0f);
-            spine = new Vector3(90f, 180f, 0f);
-            neck = new Vector3(90f, 180f, 0f);
-            head = new Vector3(90f, 180f, 0f);
-
-            legUpperLeft = new Vector3(90f, 0f, 0f);
-            legLowerLeft = new Vector3(90f, 0f, 0f);
-            footLeft = new Vector3(40f, 0f, 0f);
-
-            legUpperRight = new Vector3(90f, 0f, 0f);
-            legLowerRight = new Vector3(90f, 0f, 0f);
-            footRight = new Vector3(40f, 0f, 0f);
-
-            clavicleLeft = new Vector3(90f, 0f, 0f);
-            armUpperLeft = new Vector3(90f, 0f, 0f);
-            armLowerLeft = new Vector3(90f, 0f, 0f);
-            handLeft = new Vector3(90f, 90f, 0f);
-            thumbLeft = new Vector3(90f, 0f, 0f);
-            fingersLeft = new Vector3(90f, 0f, 0f);
-
-            clavicleRight = new Vector3(90f, 0f, 0f);
-            armUpperRight = new Vector3(90f, 0f, 0f);
-            armLowerRight = new Vector3(90f, 0f, 0f);
-            handRight = new Vector3(90f, 0f, 0f);
-            thumbRight = new Vector3(90f, 0f, 0f);
-            fingersRight = new Vector3(90f, 0f, 0f);
-        }
-    }
-    [System.Serializable]
-    public class Model3 : BoneRotations
-    {
-        public Model3()
-        {
             root = new Vector3(0f, 0f, 270f);
             hip = new Vector3(0f, 270f, 0f);
             spine = new Vector3(0f, 270f, 0f);
@@ -156,6 +118,40 @@ namespace QualisysRealTime.Unity.Skeleton
             handRight = new Vector3(0f, 0f, 0f);
             thumbRight = new Vector3(0f, 0f, 0f);
             fingersRight = new Vector3(0f, 0f, 0f);
+        }
+    }
+    [System.Serializable]
+    public class Model3 : BoneRotations
+    {
+        public Model3()
+        {
+            root = new Vector3(0f, 0f, 0f);
+            hip = new Vector3(90f, 180f, 0f);
+            spine = new Vector3(90f, 180f, 0f);
+            neck = new Vector3(90f, 180f, 0f);
+            head = new Vector3(90f, 180f, 0f);
+
+            legUpperLeft = new Vector3(90f, 0f, 0f);
+            legLowerLeft = new Vector3(90f, 0f, 0f);
+            footLeft = new Vector3(40f, 0f, 0f);
+
+            legUpperRight = new Vector3(90f, 0f, 0f);
+            legLowerRight = new Vector3(90f, 0f, 0f);
+            footRight = new Vector3(40f, 0f, 0f);
+
+            clavicleLeft = new Vector3(90f, 0f, 0f);
+            armUpperLeft = new Vector3(90f, 0f, 0f);
+            armLowerLeft = new Vector3(90f, 0f, 0f);
+            handLeft = new Vector3(90f, 90f, 0f);
+            thumbLeft = new Vector3(90f, 0f, 0f);
+            fingersLeft = new Vector3(90f, 0f, 0f);
+
+            clavicleRight = new Vector3(90f, 0f, 0f);
+            armUpperRight = new Vector3(90f, 0f, 0f);
+            armLowerRight = new Vector3(90f, 0f, 0f);
+            handRight = new Vector3(90f, 0f, 0f);
+            thumbRight = new Vector3(90f, 0f, 0f);
+            fingersRight = new Vector3(90f, 0f, 0f);
         }
     }
     [System.Serializable]
