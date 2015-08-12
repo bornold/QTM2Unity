c14552a34ad6aa4a3932e79f6fb8b20fe06bea1c
Jonas Bornold <jonas.bornold@gmail.com>
2015-08-12 12:24:44 +0200

    Adding some Markers Alias

--- Skeleton Builder/Joint Localization/MarkerNames.cs
+++ Skeleton Builder/Joint Localization/MarkerNames.cs
@@ -200,8 +200,8 @@ namespace QualisysRealTime.Unity.Skeleton
         #region hip
         public static readonly List<string>
             bodyBaseAKA = new List<string>() { "SACR", "SACRUM", "LOWER_LUMBAR", "LV5_S1", "S1" },
-            leftHipAKA = new List<string>() { "L_IAS", "L_ASIS","LASIS","LEFT_HIP","L_ICT","Lt Hip Front", "LFWT"},
-            rightHipAKA = new List<string>() { "R_IAS", "R_ASIS", "RASIS", "RIGHT_HIP", "R_ICT", "Rt Hip Front", "RFWT" },
+            leftHipAKA = new List<string>() { "L_IAS", "L_ASIS","LASIS","LEFT_HIP","L_ICT","Lt Hip Front", "LFWT", "LASI"},
+            rightHipAKA = new List<string>() { "R_IAS", "R_ASIS", "RASIS", "RIGHT_HIP", "R_ICT", "Rt Hip Front", "RFWT", "RASI" },
         #endregion
         #region upperbody
             spineAKA = new List<string>() 
@@ -211,39 +211,39 @@ namespace QualisysRealTime.Unity.Skeleton
                 "TV10", "TH10", "D10", "T10", 
                 "TV14", "TV14", "D14", "T14",
                 "L1",   "LV3"       },
-            neckAKA = new List<string>() { "TV2", "TV1", "C7", "C7_TOP_SPINE", "NECK" },
+            neckAKA = new List<string>() { "TV2", "TV1", "C7", "C7_TOP_SPINE", "NECK", "Thor1"},
             chestAKA = new List<string>() { "SME", "SJN", "CLAV" },
-            leftShoulderAKA = new List<string>() {"L_SAE", "L_ACR", "LEFT_SHOULDER", "L_SHOULDER", "L_ACROMION", "LEFTSHOULDER", "LEFT SHOULDER", "LSHO", "LSHOULDER" },
-            rightShoulderAKA = new List<string>() { "R_SAE", "R_ACR", "RIGHT_SHOULDER", "R_SHOULDER", "R_ACROMION", "RIGHTSHOULDER", "RIGHT SHOULDER", "RSHO", "RSHOULDER"} ,
+            leftShoulderAKA = new List<string>() {"L_SAE", "L_ACR", "LEFT_SHOULDER", "L_SHOULDER", "L_ACROMION", "LEFTSHOULDER", "LEFT SHOULDER", "LSHO", "LSHOULDER", "LSHOULD" },
+            rightShoulderAKA = new List<string>() { "R_SAE", "R_ACR", "RIGHT_SHOULDER", "R_SHOULDER", "R_ACROMION", "RIGHTSHOULDER", "RIGHT SHOULDER", "RSHO", "RSHOULDER", "RSHOULD" } ,
         #endregion
                 #region head
                 headAKA = new List<string>() {"SGL", "Front of Head", "F_HEAD" },
-            leftHeadAKA = new List<string>() { "L_HEAD", "HEAD_LEFT_FRONT", "Left Head", "L_HEAD", "LBHD", "HEAD2" },
-            rightHeadAKA = new List<string>() { "R_HEAD", "HEAD_RIGHT_FRONT", "Right Head", "R_HEAD", "RBHD", "HEAD1" },
+            leftHeadAKA = new List<string>() { "L_HEAD", "HEAD_LEFT_FRONT", "Left Head", "L_HEAD", "LBHD", "HEAD2", "LTEMP" },
+            rightHeadAKA = new List<string>() { "R_HEAD", "HEAD_RIGHT_FRONT", "Right Head", "R_HEAD", "RBHD", "HEAD1", "RTEMP" },
         #endregion
         #region elbow
         #region left elbow
             leftElbowAKA = new List<string>() {"L_UOA", "L_ELB", "LEFT_ELBOW" },
-            leftInnerElbowAKA = new List<string>() { "L_HME", "L_ELBOW_MED" },
-            leftOuterElbowAKA = new List<string>() { "L_HLE", "L_ELBOW", "Lt Elbow", "LELB" },
+            leftInnerElbowAKA = new List<string>() { "L_HME", "L_ELBOW_MED", "LLELBW" },
+            leftOuterElbowAKA = new List<string>() { "L_HLE", "L_ELBOW", "Lt Elbow", "LELB", "LMELBW" },
         #endregion
-        #region right elbow
-            rightElbowAKA = new List<string>() {"R_UOA", "R_ELB", "RIGHT_ELBOW" },
-            rightInnerElbowAKA = new List<string>() { "R_HME", "R_ELBOW_MED" },
-            rightOuterElbowAKA = new List<string>() { "R_HLE", "R_ELBOW", "Rt Elbow", "RELB" },
+                #region right elbow
+                rightElbowAKA = new List<string>() {"R_UOA", "R_ELB", "RIGHT_ELBOW" },
+            rightInnerElbowAKA = new List<string>() { "R_HME", "R_ELBOW_MED", "RLELBW" },
+            rightOuterElbowAKA = new List<string>() { "R_HLE", "R_ELBOW", "Rt Elbow", "RELB", "RMELBW" },
         #endregion
         #endregion
         #region hand
         #region lefthand
-            leftWristAKA = new List<string>() { "L_USP", "L_ULNA", "LEFT_WRIST_INNER", "L_WRIST_MED", "Lt Wrist Ulna", "LT Ulna", "LWRB", "L_HAND_M" },
-            leftWristRadiusAKA = new List<string>() { "L_RSP", "L_RADUIS", "LEFT_WRIST_OUTER", "L_WRIST_LAT", "Lt Wrist Radius", "Lt Radius", "LWRA", "L_HAND_L" },
+            leftWristAKA = new List<string>() { "L_USP", "L_ULNA", "LEFT_WRIST_INNER", "L_WRIST_MED", "Lt Wrist Ulna", "LT Ulna", "LWRB", "L_HAND_M", "LMWRIST" },
+            leftWristRadiusAKA = new List<string>() { "L_RSP", "L_RADUIS", "LEFT_WRIST_OUTER", "L_WRIST_LAT", "Lt Wrist Radius", "Lt Radius", "LWRA", "L_HAND_L", "LLWRIST" },
             leftHandAKA = new List<string>() { "L_HM2", "Lt 3rd Digita", "LFIN", "LHAND" },
             leftIndexAKA = new List<string>() { "L_Index", "L_INDEX1" },
             leftThumbAKA = new List<string>() { "L_Thumb","L_THUMB" },
         #endregion
         #region right hand
-            rightWristAKA = new List<string>() { "R_USP", "R_ULNA", "RIGHT_WRIST_INNER", "R_WRIST_MED", "Rt Wrist Ulna", "RT Ulna", "RWRB", "R_HAND_M" },
-            rightWristRadiusAKA = new List<string>() {"R_RSP", "R_RADUIS", "RIGHT_WRIST_OUTER", "R_WRIST_LAT", "Rt Wrist Radius" ,"RT Radius", "RWRA", "R_HAND_L" },
+            rightWristAKA = new List<string>() { "R_USP", "R_ULNA", "RIGHT_WRIST_INNER", "R_WRIST_MED", "Rt Wrist Ulna", "RT Ulna", "RWRB", "R_HAND_M","RMWRIST" },
+            rightWristRadiusAKA = new List<string>() {"R_RSP", "R_RADUIS", "RIGHT_WRIST_OUTER", "R_WRIST_LAT", "Rt Wrist Radius" ,"RT Radius", "RWRA", "R_HAND_L", "RLWRIST" },
             rightHandAKA = new List<string>() { "R_HM2", "Rt 3rd Digita", "RFIN", "RHAND" },
             rightIndexAKA = new List<string>() { "R_Index", "R_INDEX1" },
             rightThumbAKA = new List<string>() { "R_Thumb", "R_THUMB" },
@@ -252,41 +252,41 @@ namespace QualisysRealTime.Unity.Skeleton
         #region knee
         #region left knee
             leftUpperKneeAKA = new List<string>() { "L_PAS", "L_SUPPAT" },
-            leftOuterKneeAKA = new List<string>() { "L_FLE", "L_LKNEE", "LEFT_KNEE", "l_knjntln", "Lt Lat Knee", "L_KNEE_LAT", "LKNE", "LKNEE" },
-            leftInnerKneeAKA = new List<string>() { "L_FME", "L_MKNEE", "LEFT_MEDIAL_KNEE", "Lt Medial Knee", "L_KNEE_MED" },
+            leftOuterKneeAKA = new List<string>() { "L_FLE", "L_LKNEE", "LEFT_KNEE", "l_knjntln", "Lt Lat Knee", "L_KNEE_LAT", "LKNE", "LKNEE", "LLKNEE" },
+            leftInnerKneeAKA = new List<string>() { "L_FME", "L_MKNEE", "LEFT_MEDIAL_KNEE", "Lt Medial Knee", "L_KNEE_MED", "LMKNEE" },
             leftLowerKneeAKA = new List<string>() { "L_TTC", "l_tubtib", "Lt Tibia", "L_TIB_1" },
         #endregion
         #region right knee
 
             rightUpperKneeAKA = new List<string>() {"R_PAS", "R_SUPPAT" },
-            rightOuterKneeAKA = new List<string>() {"R_FLE", "R_LKNEE", "RIGHT_KNEE", "r_knjntln","Rt Lat Knee", "R_KNEE_LAT", "RKNE", "RKNEE" },
-            rightInnerKneeAKA = new List<string>() {"R_FME", "R_MKNEE", "RIGHT_MEDIAL_KNEE","Rt Medial Knee", "L_KNEE_MED" },
+            rightOuterKneeAKA = new List<string>() {"R_FLE", "R_LKNEE", "RIGHT_KNEE", "r_knjntln","Rt Lat Knee", "R_KNEE_LAT", "RKNE", "RKNEE", "RLKNEE" },
+            rightInnerKneeAKA = new List<string>() {"R_FME", "R_MKNEE", "RIGHT_MEDIAL_KNEE","Rt Medial Knee", "R_KNEE_MED", "RMKNEE" },
             rightLowerKneeAKA = new List<string>() { "R_TTC", "r_tubtib", "Rt Tibia", "R_TIB_1" },
 
         #endregion
         #endregion
         #region foot
-        #region left foot
-            leftOuterAnkleAKA = new List<string>() { "L_FAL", "L_LMAL", "LEFT_ANKLE_OUTER", "l_ankle", "Lt Ankle", "L_ANKLE_LAT", "LANK" },
-            leftInnerAnkleAKA = new List<string>() { "L_TAM", "L_MMAL", "LEFT_MEDIAL_ANKLE", "Lt Medial Ankle", "L_ANKLE_MED" },
-            leftHeelAKA = new List<string>() {"L_FCC", "L_HEEL_CALC", "LEFT_HEEL", "l_heel", "L_FCC1", "L_FCC2", "Lt Heel", "LHEE", "L_h" },
-        #endregion
+            #region left foot
+            leftOuterAnkleAKA = new List<string>() { "L_FAL", "L_LMAL", "LEFT_ANKLE_OUTER", "l_ankle", "Lt Ankle", "L_ANKLE_LAT", "LANK", "LLANK" },
+            leftInnerAnkleAKA = new List<string>() { "L_TAM", "L_MMAL", "LEFT_MEDIAL_ANKLE", "Lt Medial Ankle", "L_ANKLE_MED", "LMANK" },
+            leftHeelAKA = new List<string>() {"L_FCC", "L_HEEL_CALC", "LEFT_HEEL", "l_heel", "L_FCC1", "L_FCC2", "Lt Heel", "LHEE", "L_h", "LHEEL" },
+            #endregion
         #region right foot
-            rightOuterAnkleAKA = new List<string>() {"R_FAL", "R_LMAL", "RIGHT_ANKLE_OUTER", "l_ankle", "Rt Ankle", "R_ANKLE_LAT", "RANK" },
-            rightInnerAnkleAKA = new List<string>() { "R_TAM", "R_MMAL", "RIGHT_MEDIAL_ANKLE", "Rt Medial Ankle", "R_ANKLE_MED" },
-            rightHeelAKA = new List<string>() {"R_FCC", "R_HEEL_CALC", "RIGHT_HEEL", "r_heel", "R_FCC1", "R_FCC2", "Rt Heel", "RHEE", "R_h" },
+            rightOuterAnkleAKA = new List<string>() {"R_FAL", "R_LMAL", "RIGHT_ANKLE_OUTER", "l_ankle", "Rt Ankle", "R_ANKLE_LAT", "RANK","RLANK" },
+            rightInnerAnkleAKA = new List<string>() { "R_TAM", "R_MMAL", "RIGHT_MEDIAL_ANKLE", "Rt Medial Ankle", "R_ANKLE_MED", "RMANK" },
+            rightHeelAKA = new List<string>() {"R_FCC", "R_HEEL_CALC", "RIGHT_HEEL", "r_heel", "R_FCC1", "R_FCC2", "Rt Heel", "RHEE", "R_h", "RHEEL" },
         #endregion
-        #region toes
-        #region left toes
-            leftToe1AKA = new List<string>() { "L_FM1", "L_TOE_1_MET", "L_MT_1", "LTOE", "L_D1MT"},
+            #region toes
+            #region left toes
+            leftToe1AKA = new List<string>() { "L_FM1", "L_TOE_1_MET", "L_MT_1", "LTOE", "L_D1MT", "LFRST" },
             leftToe2AKA = new List<string>() { "L_FM2", "l_toe", "Lt 2nd Toe", "LFOOT" },
-            leftToe5AKA = new List<string>() { "L_FM5", "L_TOE_5_MET", "Lt 5th Toe", "L_MT_5", "LMT5", "L_D5MT"},
-        #endregion
-                #region right toes
-                rightToe1AKA = new List<string>() { "R_FM1", "R_TOE_1_MET", "R_MT_1", "RTOE", "R_D1MT" },
+            leftToe5AKA = new List<string>() { "L_FM5", "L_TOE_5_MET", "Lt 5th Toe", "L_MT_5", "LMT5", "L_D5MT", "LFIFTH" },
+            #endregion
+            #region right toes
+            rightToe1AKA = new List<string>() { "R_FM1", "R_TOE_1_MET", "R_MT_1", "RTOE", "R_D1MT", "RFRST" },
             rightToe2AKA = new List<string>() { "R_FM2", "r_toe","Rt 2nd Toe", "RFOOT" },
-            rightToe5AKA = new List<string>() {"R_FM5", "R_TOE_5_MET","Rt 5th Toe", "R_MT_5", "RMT5", "R_D5MT" };
-        #endregion
+            rightToe5AKA = new List<string>() {"R_FM5", "R_TOE_5_MET","Rt 5th Toe", "R_MT_5", "RMT5", "R_D5MT", "RFIFTH" };
+            #endregion
         #endregion
         #endregion
     }
